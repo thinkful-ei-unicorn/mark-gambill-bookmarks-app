4cbb1812891287a1fedc24d7276300d777da62e8
mwgambill <mwgambill@gmail.com>
2020-11-29 12:52:43 -0500

    Intial functioning upload

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
